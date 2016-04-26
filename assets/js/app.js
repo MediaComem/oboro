2268033f9469145d36875b4ff1f169452d5edd73
Simon Carlier <admin@simka.ch>
2016-04-26 13:15:35 +0200

    app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -32,7 +32,9 @@ $(function() {
 
 		//show the the div
 		this.show = function(){
-			$("#"+that.part).css("display","block");
+			$("#"+that.part).fadeIn( "fast", function() {
+			    // Animation complete
+			  });;
 		}
 
 		//hide the div
@@ -75,7 +77,9 @@ $(function() {
 
 		this.removeOthers = function(){
 			//remove all the other site parts except the current one
-		 	$(".site-part").not("."+that.part).remove();
+		 	$(".site-part").not("."+that.part).fadeOut( "fast", function() {
+			  	$(".site-part").not("."+that.part).remove();	  
+			  });
 		}
 
 		$.get(that.baseUrl+"/assets/css/"+that.part+".css", function( css ) {
