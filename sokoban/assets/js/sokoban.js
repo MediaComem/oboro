f9f62224bb09d86534c2770fd77aae46988a0cd7
Quentin Schwitzguébel <negus1309@gmail.com>
2016-04-26 09:48:13 +0200

    addfullscr

--- sokoban/assets/js/sokoban.js
+++ sokoban/assets/js/sokoban.js
@@ -705,37 +705,36 @@ else {
 
 
   if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-   // some code..
-     alert('mobile')
-     $('head').append('<link rel=\"stylesheet\" href=\"./sokoban/mqmobile.css\" />')
-     //$('head').append('<script type=\"text/javascript\" src=\"./sokoban/hide-address-bar.js\" ></script>')
 
-     //document.body.requestFullscreen();
-     function toggleFullScreen() {
-  if (!document.fullscreenElement &&    // alternative standard method
-      !document.mozFullScreenElement && !document.webkitFullscreenElement) {  // current working methods
-    if (document.documentElement.requestFullscreen) {
-      document.documentElement.requestFullscreen();
-    } else if (document.documentElement.mozRequestFullScreen) {
-      document.documentElement.mozRequestFullScreen();
-    } else if (document.documentElement.webkitRequestFullscreen) {
-      document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
-    }
-  } else {
-    if (document.cancelFullScreen) {
-      document.cancelFullScreen();
-    } else if (document.mozCancelFullScreen) {
-      document.mozCancelFullScreen();
-    } else if (document.webkitCancelFullScreen) {
-      document.webkitCancelFullScreen();
-    }
-  }
-}
+     //alert('mobile')
+     $('head').append('<link rel=\"stylesheet\" href=\"./sokoban/mqmobile.css\" />')
+
+
+     /*function toggleFullScreen() {
+        if (!document.fullscreenElement &&    // alternative standard method
+            !document.mozFullScreenElement && !document.webkitFullscreenElement) {  // current working methods
+          if (document.documentElement.requestFullscreen) {
+            document.documentElement.requestFullscreen();
+          } else if (document.documentElement.mozRequestFullScreen) {
+            document.documentElement.mozRequestFullScreen();
+          } else if (document.documentElement.webkitRequestFullscreen) {
+            document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+          }
+        } else {
+          if (document.cancelFullScreen) {
+            document.cancelFullScreen();
+          } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+          } else if (document.webkitCancelFullScreen) {
+            document.webkitCancelFullScreen();
+          }
+        }
+      }
 
      $('#arrow_bottom').on('click', function(){
 
        toggleFullScreen();
-     })
+     })*/
 
 
 
