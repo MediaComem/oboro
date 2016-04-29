88534357d99ec076a2c9f50caff198c18c48f722
Simon Carlier <admin@simka.ch>
2016-04-29 14:17:59 +0200

    changed difficulty

--- stomachjump/assets/js/stomachjump.js
+++ stomachjump/assets/js/stomachjump.js
@@ -175,9 +175,9 @@ function Platform() {
 	//3: Breakable (Go through)
 	//4: Vanishable
 	//Setting the probability of which type of platforms should be shown at what score
-	if (score >= 5000) this.types = [2, 3, 3, 3, 4, 4, 4, 4];
-	else if (score >= 2000 && score < 5000) this.types = [2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4];
-	else if (score >= 1000 && score < 2000) this.types = [3, 3, 3, 3, 3, 3, 3, 3, 3];
+	if (score >= 5000) this.types = [3, 3, 3, 3, 3, 3, 3, 3];
+	else if (score >= 2000 && score < 5000) this.types = [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3];
+	else if (score >= 1000 && score < 2000) this.types = [4, 4, 4, 4, 4, 3, 3, 3, 3];
 	else if (score >= 500 && score < 1000) this.types = [1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4];
 	else if (score >= 100 && score < 500) this.types = [1, 1, 1, 1, 2, 2];
 	else this.types = [1];
